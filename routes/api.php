82b8335ba6ddd0da50a82503de9261207f4a2ee3
mahacreative <gunturmadjid.3@gmail.com>
2022-12-23 22:29:52 +0530

    first

--- routes/api.php
+++ routes/api.php
@@ -8,12 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('jadwal_pakan', JadwalPakan::class);
 Route::get('response', [Response::class, 'index']);
-Route::post('post', function (\Illuminate\Http\Request $request) {
-    \Illuminate\Support\Facades\Storage::append(
-        "arduino-log.txt",
-        "Time: " . now()->format("Y-m-d H:i:s") . ', ' .
-            "Temperature: " . $request->get("temperature", "n/a") . '°C, ' .
-            "Humidity: " . $request->get("humidity", "n/a") . '%'
-    );
-    return 'true';
+Route::get('coba', function (\Illuminate\Http\Request $request) {
+
+    return json_encode(['abg' => 'abg']);
 });
